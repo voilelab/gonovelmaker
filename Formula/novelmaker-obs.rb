5ddaab988e7e8404f14fc115473146263ec52966
Mukyu <mudream4869@gmail.com>
2026-01-01 22:08:07 +0800

    🔄 update(novelmaker-obs): change download URL and SHA256 checksum to v0.0.6

--- Formula/novelmaker-obs.rb
+++ Formula/novelmaker-obs.rb
@@ -1,8 +1,8 @@
 class NovelmakerObs < Formula
   desc "CLI tool for managing novel projects in Obsidian vaults with OpenAI integration"
   homepage "https://github.com/voilelab/gonovelmaker"
-  url "https://github.com/voilelab/gonovelmaker/archive/refs/tags/v0.0.5.tar.gz"
-  sha256 "9f58a04686b5733dd1c0934dca36bd69acadffbcbc5867043822fe7805a5a660" # Run: shasum -a 256 <downloaded-file.tar.gz>
+  url "https://github.com/voilelab/gonovelmaker/archive/refs/tags/v0.0.6.tar.gz"
+  sha256 "5820006244a90097f8a090eedf976dfa1f09b8eaeaeb88c5b62630a4aa3f79a1" # Run: shasum -a 256 <downloaded-file.tar.gz>
   license "MIT"
   head "https://github.com/voilelab/gonovelmaker.git", branch: "main"
 
